2cbd88b6531bbf691403616385f1b3ab5ec1b147
auvm <angelurielvelascom@gmail.com>
2023-10-16 23:25:29 -0600

    initial commit

---
+++ convertir_datos.py
@@ -0,0 +1,13 @@
+edad = int(input('Ingresa tu edad: '))
+print(edad)
+print(type(edad))
+
+altura = float(input('Ingresa tu altura'))
+print(altura)
+print(type(altura))
+
+respuesta = input('Ingresa la respuesta(si/no): ')
+respuesta = True if respuesta=='si' or respuesta=='Si' or respuesta=='SI' or respuesta == 'sI' else False
+print(respuesta)
+print(type(respuesta))
+
---
+++ entrada.py
@@ -0,0 +1,6 @@
+nombre_completo = input("Ingresa tu nombre completo: ")
+
+
+print('''La función input recibe la información, en este caso: "nombre_completo",
+ y siempre va a retornar un tipo de dato ''', type(nombre_completo))
+
